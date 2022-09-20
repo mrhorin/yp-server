281bddaced24bbdf67c1598d047d48afa12955dd
mrhorin <u0813mb@gmail.com>
2022-09-21 02:13:01 +0900

    main.pyにリネーム

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import json
 import time
 import requests
@@ -6,27 +7,22 @@ import socketserver
 import threading
 import argparse
 
+# オプション
 parser = argparse.ArgumentParser()
-
-# ポート番号
 parser.add_argument("--port", "-p", type=int, default=7144 , help="使用するポート番号を指定(default: 7144)")
-# 更新間隔
 parser.add_argument("--interval", "-i", type=int, default=60 , help="index.txtの更新間隔を秒単位で指定(default: 60)")
-
 args = parser.parse_args()
 
 JSON_PATH = './yp.json'
 INTERVAL = args.interval
 PORT = args.port
 
-# yp_serverを起動
 class IndexTxtRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/index.txt':
-            self.path = '/public/index.txt'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+  def do_GET(self):
+    if self.path == '/index.txt':
+      self.path = '/public/index.txt'
+      return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-# URLからindex.txtを取得
 def get_index_txt(url):
   if url[-1] != '/':
     url + '/'
@@ -54,9 +50,11 @@ def start_updating_index_txt():
     print("index.txt is updated.")
     time.sleep(INTERVAL)
 
-t1 = threading.Thread(target=start_yp_server)
-t2 = threading.Thread(target=start_updating_index_txt)
+def main():
+  t1 = threading.Thread(target=start_yp_server)
+  t2 = threading.Thread(target=start_updating_index_txt)
+  t1.start()
+  t2.start()
 
-t1.start()
-t2.start()
-
+if __name__ == '__main__':
+  main()
